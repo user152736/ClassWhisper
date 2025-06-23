1da8b0a5493ff7584704bf4720e94d83013fa012
user152736 <mastedevorlalegutaver@gmail.com>
2025-06-23 15:24:47 +0500

    main

---
+++ main.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.types import Message
+
+TOKEN = getenv("BOT_TOKEN")
+
+
+dp = Dispatcher()
+
+
+
+async def main() -> None:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
