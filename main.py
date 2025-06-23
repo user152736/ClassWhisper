a70ab92938fc2b5649ab4258e683faaaffda403e
Jaloliddin Jumanov <jumanovjaloliddin7@gmail.com>
2025-06-23 17:29:15 +0500

    Start2

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from dotenv import load_dotenv
+from handlers import start_router
 
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
 dp = Dispatcher()
 
-
-
 async def main() -> None:
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
+    dp.include_routers(start_router)
     await dp.start_polling(bot)
 
 
