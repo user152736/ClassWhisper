b61686122cf173cbd4f19ccb70f2fbb761a81cf2
user152736 <mastedevorlalegutaver@gmail.com>
2025-06-23 15:32:08 +0500

    message deleted

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from os import getenv
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.types import Message
 
 TOKEN = getenv("BOT_TOKEN")
 
