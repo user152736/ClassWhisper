70ba0d1f42c68718e50182f6ce9fb6a2f62e283e
Jaloliddin Jumanov <jumanovjaloliddin7@gmail.com>
2025-06-24 11:32:51 +0500

    No change

--- handlers/start.py
+++ handlers/start.py
@@ -1,18 +1,11 @@
+
 from aiogram import Router, html
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-from config.config import ADMIN
-from keybord.regular.buttons import group_buttons
 
 start_router = Router()
 
 @start_router.message(CommandStart())
 async def command_start_handler(message: Message):
-    if message.from_user.id == int(ADMIN):
-        await message.answer('botga xush kelibsiz Admin' , reply_markup=group_buttons())
-    else:
-        fullname = html.bold(message.from_user.full_name)
-        await message.answer(f"Salom, {fullname}!\n\n"
-                             f"Bu bot sizga farzandigizni o'zlashtirish ko'rsatkichlari haqida xabar berib boradi!")
-
-
+    fullname = html.bold(message.from_user.full_name)
+    await message.answer(f"Salom, {fullname}!\n\n Bu bot sizga farzandigizni o'zlashtirish ko'rsatkichlari haqida xabar berib turadi!")
